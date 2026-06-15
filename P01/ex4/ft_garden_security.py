a14ba723c4a860960d7b60f7ea9165fb14f7abfc
Nelson Da Rocha Dias <nda-roch@c1r10s8.42porto.com>
2026-06-15 19:14:49 +0100

    updated ex4 and 6

--- P01/ex4/ft_garden_security.py
+++ P01/ex4/ft_garden_security.py
@@ -7,19 +7,21 @@
 #   By: nda-roch <nda-roch@student.42porto.com>      +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/06/15 14:42:48 by nda-roch            #+#    #+#            #
-#   Updated: 2026/06/15 17:57:09 by nda-roch           ###   ########.fr      #
+#   Updated: 2026/06/15 18:28:58 by nda-roch           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
 class Plant:
     def __init__(self, name, height, ages):
         self.name = name
-        self._height = height
-        self._ages = ages
+        self._height = 0
+        self.set_height(height)
+        self._ages = 0
+        self.set_age(ages)
         self.growth = 0
 
     def get_height(self):
-        return round(self._height)
+        return round(self._height, 2)
 
     def get_age(self):
         return self._ages
@@ -29,37 +31,37 @@ class Plant:
             print(f"{self.name}: Error, height can't be negative")
             print("Height update rejected")
         else:
-            self.__height = height
+            self._height = height
             print(f"Height updated: {Plant.get_height(self)}cm")
 
-    def set_age(self, age):
-        if (age < 0):
+    def set_age(self, ages):
+        if (ages < 0):
             print(f"{self.name}: Error, age can't be negative")
             print("Age update rejected")
         else:
-            self.__ages = age
+            self._ages = ages
             print(f"Age updated: {Plant.get_age(self)} days")
 
     def show(self):
         print(f"{self.name}: {round(self._height, 2)}cm, {self._ages} days old")
 
     def grow(self):
-        self.height += 0.8
+        self._height += 0.8
         self.growth += 0.8
 
     def age(self):
-        self.ages += 1
+        self._ages += 1
 
 
 if (__name__ == "__main__"):
 
     name = ["Rose", "Sunflower", "Cactus", "Sunflower", "Fern"]
     height = [15.0, 200, 5, 80, 15]
-    age = [10, 365, 90, 45, 120]
+    ages = [10, 365, 90, 45, 120]
 
     print("=== Garden Security System ===")
     for i in range(1):
-        plant = Plant(name[i], height[i], age[i])
+        plant = Plant(name[i], height[i], ages[i])
         print("Plant created: ", end="")
         plant.show()
 
