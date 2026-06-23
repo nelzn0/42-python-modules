aa1762fadc81863d2c694f149dd0f4d99d67d4a8
Nelson Dias <nelso@Nelsons-MacBook-Air.local>
2026-06-23 12:44:54 +0100

    Updated P02 ex0 and 1

--- P02/ex1/ft_raise_exception.py
+++ P02/ex1/ft_raise_exception.py
@@ -7,7 +7,7 @@
 #   By: nda-roch <nda-roch@student.42porto.com>      +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/06/23 11:22:18 by nda-roch            #+#    #+#            #
-#   Updated: 2026/06/23 11:48:50 by nda-roch           ###   ########.fr      #
+#   Updated: 2026/06/23 12:44:31 by nda-roch           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
@@ -16,9 +16,9 @@ def input_temperature(temp_str: str) -> int:
     print(f"Input data is '{temp_str}'")
     temp_int = int(temp_str)
     if (temp_int > 40):
-        raise Exception
+        raise ValueError(f"{temp_int}°C is too hot for plants (max 40°C)")
     elif (temp_int < 0):
-        raise Exception
+        raise ValueError(f"{temp_int}°C is too cold for plants (min 0°C)")
     print(f"Temperature is now {temp_int}°C")
     return temp_int
 
@@ -31,15 +31,14 @@ def test_temperature() -> None:
         print(f"Caught input_temperature error: {e}")
     try:
         input_temperature("100")
-    except:
-        print("Caught input_temperature error: 100°C is too hot for plants (max 40°C)")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
     try:
         input_temperature("-50")
-    except:
-        print("Caught input_temperature error: -50°C is too cold for plants (min 0°C)")
-    finally:
-        print("")
-        print("All tests completed - program didn't crash!")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+    print("")
+    print("All tests completed - program didn't crash!")
 
 
 if (__name__ == "__main__"):
