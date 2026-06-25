02494d437d4292ba2fc7f8d0815ab6861c72d3aa
Nelson Da Rocha Dias <nda-roch@c1r10s9.42porto.com>
2026-06-25 17:23:39 +0100

    progress on P3

--- P03/ex1/ft_score_analytics.py
+++ P03/ex1/ft_score_analytics.py
@@ -2,27 +2,30 @@
 # ########################################################################### #
 #   shebang: 1                                                                #
 #                                                          :::      ::::::::  #
-#   ft_command_quest.py                                  :+:      :+:    :+:  #
+#   ft_score_analytics.py                                :+:      :+:    :+:  #
 #                                                      +:+ +:+         +:+    #
 #   By: nda-roch <nda-roch@student.42porto.com>      +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
-#   Created: 2026/06/25 15:58:24 by nda-roch            #+#    #+#            #
-#   Updated: 2026/06/25 16:49:03 by nda-roch           ###   ########.fr      #
+#   Created: 2026/06/25 16:53:15 by nda-roch            #+#    #+#            #
+#   Updated: 2026/06/25 17:11:21 by nda-roch           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
 import sys
 
 if (__name__ == "__main__"):
-    print("=== Command Quest ===")
-    print(f"Program name: {sys.argv[0]}")
+    print("=== Player Score Analytics ===")
     argc = len(sys.argv)
     if argc > 1:
-        print(f"Arguments received: {argc}")
+        print(f"Total players: {argc - 1}")
+        print(f"Total score: {sum(sys.argv)}")
+        print(f"Average score: {sum(sys.argv)}")
+        print(f"High score: {max(sys.argv)}")
+        print(f"Low score: {sum(sys.argv)}")
         i = 1
         while i < argc:
             print(f"Argument {i}: {sys.argv[i]}")
             i += 1
     else:
-        print("No arguments provided!")
-    print(f"Total arguments: {argc}")
+        print(
+            "No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ...")
