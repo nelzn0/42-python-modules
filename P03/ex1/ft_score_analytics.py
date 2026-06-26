4a394e678a1ae24531e39935e4ccf6dd1698ecdc
Nelson Da Rocha Dias <nda-roch@c1r10s7.42porto.com>
2026-06-26 12:38:34 +0100

    Added ex1

--- P03/ex1/ft_score_analytics.py
+++ P03/ex1/ft_score_analytics.py
@@ -7,25 +7,41 @@
 #   By: nda-roch <nda-roch@student.42porto.com>      +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/06/25 16:53:15 by nda-roch            #+#    #+#            #
-#   Updated: 2026/06/25 17:11:21 by nda-roch           ###   ########.fr      #
+#   Updated: 2026/06/26 12:38:13 by nda-roch           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
 import sys
 
+
+def convert_str(args: list, argc: int) -> list:
+    i = 1
+    lst = []
+    while i <= argc:
+        try:
+            lst += [int(args[i])]
+        except ValueError:
+            print(f"Invalid parameter: '{sys.argv[i]}'")
+        i += 1
+    return lst
+
+
 if (__name__ == "__main__"):
+
+    argc = len(sys.argv) - 1
+    lst = []
+
     print("=== Player Score Analytics ===")
-    argc = len(sys.argv)
-    if argc > 1:
-        print(f"Total players: {argc - 1}")
-        print(f"Total score: {sum(sys.argv)}")
-        print(f"Average score: {sum(sys.argv)}")
-        print(f"High score: {max(sys.argv)}")
-        print(f"Low score: {sum(sys.argv)}")
-        i = 1
-        while i < argc:
-            print(f"Argument {i}: {sys.argv[i]}")
-            i += 1
+
+    lst = convert_str(sys.argv, argc)
+    if len(lst) > 0:
+        print(f"Scores processed: {lst}")
+        print(f"Total players: {len(lst)}")
+        print(f"Total score: {sum(lst)}")
+        print(f"Average score: {sum(lst) / len(lst)}")
+        print(f"High score: {max(lst)}")
+        print(f"Low score: {min(lst)}")
+        print(f"Score range: {max(lst) - min(lst)}")
     else:
-        print(
-            "No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+        print("No scores provided. Usage: "
+              "python3 ft_score_analytics.py <score1> <score2> ...")
