c1d5e7981bce943dc6b5a816485eaeae37d38ce3
Nelson Da Rocha Dias <nda-roch@c1r10s7.42porto.com>
2026-06-29 18:42:08 +0100

    added P05

---
+++ P05/ex0/data_processor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   data_processor.py                                    :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: nda-roch <nda-roch@student.42porto.com>      +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/06/29 18:21:39 by nda-roch            #+#    #+#            #
+#   Updated: 2026/06/29 18:41:56 by nda-roch           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+import typing
+import abc
+
+if (__name__ == "__main__"):
+    print("=== Code Nexus - Data Processor ===")
